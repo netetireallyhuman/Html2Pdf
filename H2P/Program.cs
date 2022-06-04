667c60b94b0bf11ea678150fa1f5660bab4eb723
Erik Nagel <erik@reallyhuman.net>
2022-06-04 16:04:21 +0200

    demo added.

--- H2P/Program.cs
+++ H2P/Program.cs
@@ -225,17 +225,16 @@ namespace H2P
             fullMessage.AppendLine(" [options] html-filename(locally saved html-file) pdf-filename[.pdf]");
             fullMessage.AppendLine("\tOptions: -f(force overwriting)");
             fullMessage.AppendLine("\t         -n(no header, no footer)");
-            fullMessage.AppendLine("\t         -p:page-settings(comma-separated)");
+            fullMessage.AppendLine("\t         -p:page-setting[=value]");
             fullMessage.AppendLine("\t         page-settings: ");
-            fullMessage.AppendLine("\t\t\tA4: Din-A4 portrait (default).");
-            fullMessage.AppendLine("\t\t\tl or landscape: Din-A4 landscape.");
+            fullMessage.AppendLine("\t\t\tl or landscape: din-A4 landscape (default: din-A4 portrait).");
             fullMessage.AppendLine("\t\t\tpw or paperWidth=paper-width (default:210.0mm).");
             fullMessage.AppendLine("\t\t\tph or paperHeight=paper-height  (default:297.0mm).");
             fullMessage.AppendLine("\t\t\tml or marginLeft=margin-left (default:10.0 mm).");
             fullMessage.AppendLine("\t\t\tht or headerTemplate=optional: \"header-template\"");
             fullMessage.AppendLine("\t\t\tft or footerTemplate=optional: \"footer-template\"");
             fullMessage.AppendLine("\t\t\tsc or scale=scale (default:0.9).");
-            fullMessage.AppendLine("\t\t\tpr or pageRanges=\"optional: e.g. 1 or 2-4\"");
+            fullMessage.AppendLine("\t\t\tpr or pageRanges=\"e.g. 1 or 2-4\"");
             fullMessage.AppendLine("\t         -l:locator-type=\"locator-string\"");
             fullMessage.AppendLine("\t         locator-types: ");
             fullMessage.AppendLine("\t\t\tClassName: Locates elements whose class name contains the search value.");
@@ -246,7 +245,9 @@ namespace H2P
             fullMessage.AppendLine("\t\t\tPartialLinkText: Locates anchor elements whose visible text contains the search value.");
             fullMessage.AppendLine("\t\t\tTagName: Locates elements whose tag name matches the search value.");
             fullMessage.AppendLine("\t\t\tXpath: Locates elements matching an XPath expression.");
-
+            fullMessage.AppendLine("Examples:");
+            fullMessage.AppendLine("-f -l:Xpath=\"//div[@class='VWDcomp WE021']//span[@class='price']\" -p:l -p:sc=0.95 -p:ml=20 -p:pr=\"1-3\" \"https://www.tagesschau.de/wirtschaft/boersenkurse/basf-aktie-basf11/\" basf");
+            fullMessage.AppendLine("-f demo/index.html converted.pdf");
             MessageAndOut(fullMessage.ToString());
         }
 
